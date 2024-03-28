775d147229eb2276a1d595557a1ef6dfa8ac937e
Afif Arfan <125743442+A111fault@users.noreply.github.com>
2024-03-28 15:16:39 +0600

    Add files via upload

--- experiment.sql
+++ experiment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 23, 2024 at 07:39 AM
+-- Generation Time: Mar 28, 2024 at 10:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,13 +41,9 @@ CREATE TABLE `crud` (
 --
 
 INSERT INTO `crud` (`id`, `name`, `email`, `mobile`, `password`, `user_type`) VALUES
-(51, 'Fault_a111', 'fault@gmail.com', '455667', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
-(52, 'Afif Arfan', 'afif@gmail.com', '01759813343', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
-(53, 'Safin', 'safin@gmail.com', '019221678', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
-(54, 'NA Nishat', 'nishat@gmail.com', '01681451', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
-(56, 'Rumi', 'rumi@gmail.com', '01552478', '81dc9bdb52d04dc20036dbd8313ed055', 'Employee'),
-(57, 'Shaneen Shadman', 'shaneen@gmail.com', '1234', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
-(59, 'Niloy Chowdhury', 'nilon@gmail.com', '1234', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin');
+(59, 'Niloy Chowdhury', 'nilon@gmail.com', '1234', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
+(60, 'Afif Arfan', 'tech360.afif@gmail.com', '01759813343', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
+(61, 'Teresa Palmar', 'tech360.teresa@gmail.com', '44441', '81dc9bdb52d04dc20036dbd8313ed055', 'Employee');
 
 -- --------------------------------------------------------
 
@@ -57,51 +53,54 @@ INSERT INTO `crud` (`id`, `name`, `email`, `mobile`, `password`, `user_type`) VA
 
 CREATE TABLE `inbound` (
   `SN` int(3) NOT NULL,
-  `item_id` varchar(6) NOT NULL,
+  `trx_id` varchar(1000) NOT NULL,
+  `item_id` varchar(12) NOT NULL,
   `item_description` varchar(50) NOT NULL,
   `item_quantity` varchar(100) NOT NULL,
   `unit_price` varchar(500) NOT NULL,
   `date_received` date NOT NULL,
   `supplier` varchar(50) NOT NULL,
   `total_price` varchar(100) NOT NULL,
-  `remarks` varchar(500) NOT NULL
+  `remarks` varchar(500) NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  `updated_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `inbound`
 --
 
-INSERT INTO `inbound` (`SN`, `item_id`, `item_description`, `item_quantity`, `unit_price`, `date_received`, `supplier`, `total_price`, `remarks`) VALUES
-(1, '101', 'star screws packet', '100', '1000', '2024-01-24', 'Hooks', '100000', 'intact'),
-(2, '102', 'philip screws packet', '100', '1000', '2024-01-25', 'Alamdina hardware store', '100000', 'intact'),
-(3, '103', 'Hex screws packet', '100', '1000', '2024-01-26', 'Rahim hardware store', '100000', 'intact'),
-(4, '104', 'Zip ties packet', '100', '1000', '2024-01-27', 'Nirob hardware', '100000', 'intact'),
-(5, '105', 'Flat screw driver', '100', '5000', '2024-01-28', 'Jessore hardware', '500000', 'intact'),
-(6, '106', 'Philip screw driver', '100', '10000', '2024-01-29', 'Jashim hardware', '100000', 'intact'),
-(7, '107', 'Hex Keys', '100', '20000', '2024-01-30', 'Rahima hardware', '20000', 'intact'),
-(8, '108', 'Routers', '50', '100000', '2024-01-31', 'Megna hardware', '5000000', 'intact'),
-(9, '109', 'Cisco modules', '50', '500000', '2024-02-02', 'ACI hardware', '25000000', 'intact'),
-(10, '110', 'Wrenches', '100', '30000', '2024-02-04', 'Huawei hardware', '3000000', 'intact'),
-(11, '111', 'Testers', '100', '50000', '2024-02-06', 'Robi hardware', '5000000', 'intact'),
-(12, '112', 'Drill machine', '20', '50000', '2024-02-01', 'Airtel hardware', '1000000', 'intact'),
-(13, '114', 'Optical attenuators', '1000', '40000', '2024-01-04', 'Banglalink hardware', '4000000', 'intact'),
-(14, '115', 'Measuring tape', '100', '5000', '2024-03-01', 'Double A hardware', '500000', 'intact'),
-(15, '116', 'Electro tape', '1000', '5000', '2024-02-05', 'B2B hardware', '5000000', 'intact'),
-(16, '117', 'Marking tape', '1000', '5000', '2024-02-05', 'Hanif hardware', '5000000', 'intact'),
-(17, '201', 'Marsking tape', '1000', '5000', '2024-02-06', 'Azwad hardware', '5000000', 'intact'),
-(18, '202', 'Side cutters', '150', '50000', '2024-01-23', 'Veloce hardware', '750000', 'intact'),
-(19, '203', 'Sensors', '1000', '50000', '2024-01-27', 'Sony hardware', '50000000', 'intact'),
-(20, '302', 'Repeaters', '50', '45000', '2024-01-29', 'Sony hardware', '2250000', 'intact'),
-(21, '303', 'Fiber Optic cable', '1000', '35000', '2024-01-20', 'Sony hardware', '35000000', 'intact'),
-(22, '304', 'Fiber Optic pigtail', '1000', '50000', '2024-01-21', 'Sony hardware', '500000000', 'intact'),
-(23, '305', 'FBT coupler', '200', '67000', '2024-01-15', 'Mehgna hardware', '13400000', 'intact'),
-(24, '333', 'PLC splitter', '200', '42000', '2024-01-16', 'MRF hardware', '8400000', 'intact'),
-(25, '345', 'Fast connector', '500', '32000', '2024-02-09', 'Trek hardware', '16000000', 'intact'),
-(26, '355', 'FTTH Box', '500', '68000', '2024-01-09', 'Instar hardware', '340000000', 'intact'),
-(27, '366', 'Fiber Optic closure', '5000', '20500', '2024-01-10', 'Techlogic hardware', '10250000', 'intact'),
-(28, '234', 'Modular Crimps tools', '30', '65000', '2024-02-08', 'Fare tech hardware', '1950000', 'intact'),
-(29, '255', 'CAT-5 cables', '1000', '43000', '2024-01-28', 'Aqme hardware', '43000000', 'intact'),
-(30, '245', 'Codless', '50', '154500', '2024-02-29', 'Pran hardware', '7725000', 'intact');
+INSERT INTO `inbound` (`SN`, `trx_id`, `item_id`, `item_description`, `item_quantity`, `unit_price`, `date_received`, `supplier`, `total_price`, `remarks`, `updated_by`, `updated_time`) VALUES
+(1, '1001', '101', 'star screws packet', '100', '1000', '2024-01-24', 'Hooks', '100000', 'intact', 'Niloy Chowdhury', '2024-03-28 08:55:04'),
+(2, '1002', '102', 'philip screws packet', '100', '1000', '2024-01-25', 'Alamdina hardware store', '100000', 'intact', '', '2024-03-28 08:36:43'),
+(3, '1003', '103', 'Hex screws packet', '100', '1000', '2024-01-26', 'Rahim hardware store', '100000', 'intact', '', '2024-03-28 08:36:43'),
+(4, '1004', '104', 'Zip ties packet', '100', '1000', '2024-01-27', 'Nirob hardware', '100000', 'intact', '', '2024-03-28 08:36:43'),
+(5, '1008', '105', 'Flat screw driver', '100', '5000', '2024-01-28', 'Jessore hardware', '500000', 'intact', '', '2024-03-28 08:36:43'),
+(6, '2085', '106', 'Philip screw driver', '100', '10000', '2024-01-29', 'Jashim hardware', '100000', 'intact', '', '2024-03-28 08:36:43'),
+(7, '2008', '107', 'Hex Keys', '100', '20000', '2024-01-30', 'Rahima hardware', '20000', 'intact', '', '2024-03-28 08:36:43'),
+(8, '1088', '108', 'Routers', '50', '100000', '2024-01-31', 'Megna hardware', '5000000', 'intact', '', '2024-03-28 08:36:43'),
+(9, '1077', '109', 'Cisco modules', '50', '500000', '2024-02-02', 'ACI hardware', '25000000', 'intact', '', '2024-03-28 08:36:43'),
+(10, '2086', '110', 'Wrenches', '100', '30000', '2024-02-04', 'Huawei hardware', '3000000', 'intact', '', '2024-03-28 08:36:43'),
+(11, '3043', '111', 'Testers', '100', '50000', '2024-02-06', 'Robi hardware', '5000000', 'intact', '', '2024-03-28 08:36:43'),
+(12, '4076', '112', 'Drill machine', '20', '50000', '2024-02-01', 'Airtel hardware', '1000000', 'intact', '', '2024-03-28 08:36:43'),
+(13, '2019', '114', 'Optical attenuators', '1000', '40000', '2024-01-04', 'Banglalink hardware', '4000000', 'intact', '', '2024-03-28 08:36:43'),
+(14, '2009', '115', 'Measuring tape', '100', '5000', '2024-03-01', 'Double A hardware', '500000', 'intact', '', '2024-03-28 08:36:43'),
+(15, '1012', '116', 'Electro tape', '1000', '5000', '2024-02-05', 'B2B hardware', '5000000', 'intact', '', '2024-03-28 08:36:43'),
+(16, '1099', '117', 'Marking tape', '1000', '5000', '2024-02-05', 'Hanif hardware', '5000000', 'intact', '', '2024-03-28 08:36:43'),
+(17, '1075', '201', 'Marsking tape', '1000', '5000', '2024-02-06', 'Azwad hardware', '5000000', 'intact', '', '2024-03-28 08:36:43'),
+(18, '1068', '202', 'Side cutters', '150', '50000', '2024-01-23', 'Veloce hardware', '750000', 'intact', '', '2024-03-28 08:36:43'),
+(19, '1069', '203', 'Sensors', '1000', '50000', '2024-01-27', 'Sony hardware', '50000000', 'intact', '', '2024-03-28 08:36:43'),
+(20, '1459', '302', 'Repeaters', '50', '45000', '2024-01-29', 'Sony hardware', '2250000', 'intact', '', '2024-03-28 08:36:43'),
+(21, '1980', '303', 'Fiber Optic cable', '1000', '35000', '2024-01-20', 'Sony hardware', '35000000', 'intact', '', '2024-03-28 08:36:43'),
+(22, '2014', '304', 'Fiber Optic pigtail', '1000', '50000', '2024-01-21', 'Sony hardware', '500000000', 'intact', '', '2024-03-28 08:36:43'),
+(23, '2018', '305', 'FBT coupler', '200', '67000', '2024-01-15', 'Mehgna hardware', '13400000', 'intact', '', '2024-03-28 08:36:43'),
+(24, '2044', '333', 'PLC splitter', '200', '42000', '2024-01-16', 'MRF hardware', '8400000', 'intact', '', '2024-03-28 08:36:43'),
+(25, '3034', '345', 'Fast connector', '500', '32000', '2024-02-09', 'Trek hardware', '16000000', 'intact', '', '2024-03-28 08:36:43'),
+(26, '3033', '355', 'FTTH Box', '500', '68000', '2024-01-09', 'Instar hardware', '340000000', 'intact', '', '2024-03-28 08:36:43'),
+(27, '3333', '366', 'Fiber Optic closure', '5000', '20500', '2024-01-10', 'Techlogic hardware', '10250000', 'intact', '', '2024-03-28 08:36:43'),
+(28, '4444', '234', 'Modular Crimps tools', '30', '65000', '2024-02-08', 'Fare tech hardware', '1950000', 'intact', '', '2024-03-28 08:36:43'),
+(29, '5657', '255', 'CAT-5 cables', '1000', '43000', '2024-01-28', 'Aqme hardware', '43000000', 'intact', '', '2024-03-28 08:36:43'),
+(30, '2347', '245', 'Codless', '50', '154500', '2024-02-29', 'Pran hardware', '7725000', 'intact', '', '2024-03-28 08:36:43');
 
 -- --------------------------------------------------------
 
@@ -110,7 +109,8 @@ INSERT INTO `inbound` (`SN`, `item_id`, `item_description`, `item_quantity`, `un
 --
 
 CREATE TABLE `outbound` (
-  `SN` int(3) NOT NULL,
+  `SN` int(11) NOT NULL,
+  `trx_id` varchar(1000) NOT NULL,
   `item_id` varchar(6) NOT NULL,
   `item_description` varchar(50) NOT NULL,
   `item_quantity` varchar(100) NOT NULL,
@@ -119,44 +119,46 @@ CREATE TABLE `outbound` (
   `department` varchar(50) NOT NULL,
   `destination` varchar(50) NOT NULL,
   `total_price` varchar(100) NOT NULL,
-  `remarks` varchar(500) NOT NULL
+  `remarks` varchar(500) NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  `updated_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `outbound`
 --
 
-INSERT INTO `outbound` (`SN`, `item_id`, `item_description`, `item_quantity`, `unit_price`, `date_shipped`, `department`, `destination`, `total_price`, `remarks`) VALUES
-(1, '104', 'CAT-5 cables', '100', '20', '2024-01-24', 'Logistics', 'Dhanmondi', 'tk 2000', 'intact'),
-(2, '102', 'philip screws packet', '100', '1000', '2024-01-25', 'SAS', 'Uttara', '100000', 'intact'),
-(3, '103', 'Hex screws packet', '100', '1000', '2024-01-26', 'HR', 'Banani', '100000', 'intact'),
-(4, '104', 'Zip ties packet', '100', '1000', '2024-01-27', 'Logistics', 'Mirpur-11', '100000', 'intact'),
-(5, '105', 'Flat screw driver', '100', '5000', '2024-01-28', 'SAS', 'Old Dhaka', '500000', 'intact'),
-(6, '106', 'Philip screw driver', '100', '10000', '2024-01-29', 'HR', 'Manikganj', '100000', 'intact'),
-(7, '107', 'Hex Keys', '100', '20000', '2024-01-30', 'Logistics', 'Savar', '20000', 'intact'),
-(8, '108', 'Routers', '50', '100000', '2024-01-31', 'SAS', 'Demra', '5000000', 'intact'),
-(9, '109', 'Cisco modules', '50', '500000', '2024-02-02', 'Logistics', 'Aftab Nagar', '25000000', 'intact'),
-(10, '110', 'Wrenches', '100', '30000', '2024-02-04', 'SAS', 'Kafrul', '3000000', 'intact'),
-(11, '111', 'Testers', '100', '50000', '2024-02-06', 'HR', 'Mirpur-1', '5000000', 'intact'),
-(12, '112', 'Drill machine', '20', '50000', '2024-02-01', 'Logistics', 'Postokola', '1000000', 'intact'),
-(13, '114', 'Optical attenuators', '1000', '40000', '2024-01-04', 'SAS', 'TSC', '4000000', 'intact'),
-(14, '115', 'Measuring tape', '100', '5000', '2024-03-01', 'WAP', 'Farmgate', '500000', 'intact'),
-(15, '116', 'Electro tape', '1000', '5000', '2024-02-05', 'Logistics', 'Shahbag', '5000000', 'intact'),
-(16, '117', 'Marking tape', '1000', '5000', '2024-02-05', 'WAP', 'Keraniganj', '5000000', 'intact'),
-(17, '201', 'Marsking tape', '1000', '5000', '2024-02-06', 'SAS', 'Narayanganj', '5000000', 'intact'),
-(18, '202', 'Side cutters', '150', '50000', '2024-01-23', 'WAP', 'Rupganj', '750000', 'intact'),
-(19, '203', 'Sensors', '1000', '50000', '2024-01-27', 'Logistics', 'Gazipur', '50000000', 'intact'),
-(20, '302', 'Repeaters', '50', '45000', '2024-01-29', 'HR', 'Airport', '2250000', 'intact'),
-(21, '303', 'Fiber Optic cable', '1000', '35000', '2024-01-20', 'WAP', 'Sreenagar', '35000000', 'intact'),
-(22, '304', 'Fiber Optic pigtail', '1000', '50000', '2024-01-21', 'SAS', 'Nayapur', '500000000', 'intact'),
-(23, '305', 'FBT coupler', '200', '67000', '2024-01-15', 'HR', 'Faridpur', '13400000', 'intact'),
-(24, '333', 'PLC splitter', '200', '42000', '2024-01-16', 'Logistics', 'Natore', '8400000', 'intact'),
-(25, '345', 'Fast connector', '500', '32000', '2024-02-09', 'Logistics', 'Bogura', '16000000', 'intact'),
-(26, '355', 'FTTH Box', '500', '68000', '2024-01-09', 'WAP', 'Jamalpur', '340000000', 'intact'),
-(27, '366', 'Fiber Optic closure', '5000', '20500', '2024-01-10', 'PO', 'Sunamganj', '10250000', 'intact'),
-(28, '234', 'Modular Crimps tools', '30', '65000', '2024-02-08', 'Logistics', 'Gobindaganj', '1950000', 'intact'),
-(29, '255', 'CAT-5 cables', '1000', '43000', '2024-01-28', 'HR', 'Bhatipara', '43000000', 'intact'),
-(30, '245', 'Codless', '50', '154500', '2024-02-29', 'SAS', 'Netrokona', '7725000', 'intact');
+INSERT INTO `outbound` (`SN`, `trx_id`, `item_id`, `item_description`, `item_quantity`, `unit_price`, `date_shipped`, `department`, `destination`, `total_price`, `remarks`, `updated_by`, `updated_time`) VALUES
+(1, '1001', '104', 'CAT-5 cables', '100', '20', '2024-01-24', 'Logistics', 'Dhanmondi', 'tk 2000', 'intact', '', '2024-03-28 08:36:14'),
+(2, '1002', '102', 'philip screws packet', '100', '1000', '2024-01-25', 'SAS', 'Uttara', '100000', 'intact', '', '2024-03-28 08:36:14'),
+(3, '1003', '103', 'Hex screws packet', '100', '1000', '2024-01-26', 'HR', 'Banani', '100000', 'intact', '', '2024-03-28 08:36:14'),
+(4, '1004', '104', 'Zip ties packet', '100', '1000', '2024-01-27', 'Logistics', 'Mirpur-11', '100000', 'intact', '', '2024-03-28 08:36:14'),
+(5, '1008', '105', 'Flat screw driver', '100', '5000', '2024-01-28', 'SAS', 'Old Dhaka', '500000', 'intact', '', '2024-03-28 08:36:14'),
+(6, '2085', '106', 'Philip screw driver', '100', '10000', '2024-01-29', 'HR', 'Manikganj', '100000', 'intact', '', '2024-03-28 08:36:14'),
+(7, '2008', '107', 'Hex Keys', '100', '20000', '2024-01-30', 'Logistics', 'Savar', '20000', 'intact', '', '2024-03-28 08:36:14'),
+(8, '1088', '108', 'Routers', '50', '100000', '2024-01-31', 'SAS', 'Demra', '5000000', 'intact', '', '2024-03-28 08:36:14'),
+(9, '1077', '109', 'Cisco modules', '50', '500000', '2024-02-02', 'Logistics', 'Aftab Nagar', '25000000', 'intact', '', '2024-03-28 08:36:14'),
+(10, '2086', '110', 'Wrenches', '100', '30000', '2024-02-04', 'SAS', 'Kafrul', '3000000', 'intact', '', '2024-03-28 08:36:14'),
+(11, '3043', '111', 'Testers', '100', '50000', '2024-02-06', 'HR', 'Mirpur-1', '5000000', 'intact', '', '2024-03-28 08:36:14'),
+(12, '4076', '112', 'Drill machine', '20', '50000', '2024-02-01', 'Logistics', 'Postokola', '1000000', 'intact', '', '2024-03-28 08:36:14'),
+(13, '2019', '114', 'Optical attenuators', '1000', '40000', '2024-01-04', 'SAS', 'TSC', '4000000', 'intact', '', '2024-03-28 08:36:14'),
+(14, '2009', '115', 'Measuring tape', '100', '5000', '2024-03-01', 'WAP', 'Farmgate', '500000', 'intact', '', '2024-03-28 08:36:14'),
+(15, '1012', '116', 'Electro tape', '1000', '5000', '2024-02-05', 'Logistics', 'Shahbag', '5000000', 'intact', '', '2024-03-28 08:36:14'),
+(16, '1099', '117', 'Marking tape', '1000', '5000', '2024-02-05', 'WAP', 'Keraniganj', '5000000', 'intact', '', '2024-03-28 08:36:14'),
+(17, '1075', '201', 'Marsking tape', '1000', '5000', '2024-02-06', 'SAS', 'Narayanganj', '5000000', 'intact', '', '2024-03-28 08:36:14'),
+(18, '1068', '202', 'Side cutters', '150', '50000', '2024-01-23', 'WAP', 'Rupganj', '750000', 'intact', '', '2024-03-28 08:36:14'),
+(19, '1069', '203', 'Sensors', '1000', '50000', '2024-01-27', 'Logistics', 'Gazipur', '50000000', 'intact', '', '2024-03-28 08:36:14'),
+(20, '1459', '302', 'Repeaters', '50', '45000', '2024-01-29', 'HR', 'Airport', '2250000', 'intact', '', '2024-03-28 08:36:14'),
+(21, '1980', '303', 'Fiber Optic cable', '1000', '35000', '2024-01-20', 'WAP', 'Sreenagar', '35000000', 'intact', '', '2024-03-28 08:36:14'),
+(22, '2014', '304', 'Fiber Optic pigtail', '1000', '50000', '2024-01-21', 'SAS', 'Nayapur', '500000000', 'intact', '', '2024-03-28 08:36:14'),
+(23, '2018', '305', 'FBT coupler', '200', '67000', '2024-01-15', 'HR', 'Faridpur', '13400000', 'intact', '', '2024-03-28 08:36:14'),
+(24, '2044', '333', 'PLC splitter', '200', '42000', '2024-01-16', 'Logistics', 'Natore', '8400000', 'intact', '', '2024-03-28 08:36:14'),
+(25, '3034', '345', 'Fast connector', '500', '32000', '2024-02-09', 'Logistics', 'Bogura', '16000000', 'intact', '', '2024-03-28 08:36:14'),
+(26, '3033', '355', 'FTTH Box', '500', '68000', '2024-01-09', 'WAP', 'Jamalpur', '340000000', 'intact', '', '2024-03-28 08:36:14'),
+(27, '3333', '366', 'Fiber Optic closure', '5000', '20500', '2024-01-10', 'PO', 'Sunamganj', '10250000', 'intact', '', '2024-03-28 08:36:14'),
+(28, '4444', '234', 'Modular Crimps tools', '30', '65000', '2024-02-08', 'Logistics', 'Gobindaganj', '1950000', 'intact', '', '2024-03-28 08:36:14'),
+(29, '5657', '255', 'CAT-5 cables', '1000', '43000', '2024-01-28', 'HR', 'Bhatipara', '43000000', 'intact', '', '2024-03-28 08:36:14'),
+(30, '2347', '245', 'Codless', '50', '154500', '2024-02-29', 'SAS', 'Netrokona', '7725000', 'intact', '', '2024-03-28 08:36:14');
 
 --
 -- Indexes for dumped tables
@@ -188,7 +190,7 @@ ALTER TABLE `outbound`
 -- AUTO_INCREMENT for table `crud`
 --
 ALTER TABLE `crud`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `inbound`
@@ -200,7 +202,7 @@ ALTER TABLE `inbound`
 -- AUTO_INCREMENT for table `outbound`
 --
 ALTER TABLE `outbound`
-  MODIFY `SN` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `SN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
